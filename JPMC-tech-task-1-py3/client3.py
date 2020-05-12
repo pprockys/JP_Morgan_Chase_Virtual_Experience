5c378fa8f1fedc8328ae88c004fa355c3d86b2ae
Prateek Pandey <46824331+pprockys@users.noreply.github.com>
2020-05-12 16:46:16 +0530

    TASK_1

--- JPMC-tech-task-1-py3/client3.py
+++ JPMC-tech-task-1-py3/client3.py
@@ -17,7 +17,9 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 #  DEALINGS IN THE SOFTWARE.
-
+#-------------------------------------------------------------------------------------------------------------------------------------
+#Changed by Prateek Pandey(pprockys)
+#-------------------------------------------------------------------------------------------------------------------------------------
 import urllib.request
 import time
 import json
